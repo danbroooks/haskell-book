danbroooks/haskell-comonads

Initial commit of code with test

test/test.hs
@@ -1,0 +1,14 @@
+module Test where
+
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, testCase)
+
+tests :: TestTree
+tests = testGroup
+  "tests" [
+    testCase "true"
+      $ assertEqual "true" True True
+  ]
+
+main :: IO ()
+main = defaultMain tests

danbroooks/haskell-book

test/test.hs
@@ -1,0 +1,14 @@
+module Main where
+
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, testCase)
+
+tests :: TestTree
+tests = testGroup
+  "tests" [
+    testCase "true"
+      $ assertEqual "true" True True
+  ]
+
+main :: IO ()
+main = defaultMain tests