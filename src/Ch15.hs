e1b5cfe7e30416c9073a369e7e36b08359c874e8
Dan Brooks <dmbrooks@live.co.uk>
2017-09-14 14:06:23 +0100

    Solution for second monoid exercise

--- src/Ch15.hs
+++ src/Ch15.hs
@@ -1,5 +1,7 @@
 module Ch15 where
 
+import Data.Monoid
+
 data Optional a =
   Nada
   | Only a
@@ -10,4 +12,36 @@ instance Monoid a => Monoid (Optional a) where
   mappend a Nada = a
   mappend Nada a = a
   mappend (Only a) (Only b) = Only $ a `mappend` b
+
+--
+
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+madlibbin' :: Exclamation
+           -> Adverb
+           -> Noun
+           -> Adjective
+           -> String
+madlibbin' e adv noun adj =
+  e <> "! he said " <>
+  adv <> " as he jumped into his car " <>
+  noun <> " and drove off with his " <>
+  adj <> " wife."
+
+madlibbinBetter' :: Exclamation
+           -> Adverb
+           -> Noun
+           -> Adjective
+           -> String
+madlibbinBetter' e adv noun adj =
+  mconcat [
+    e, "! he said ",
+    adv, " as he jumped into his car ",
+    noun, " and drove off with his ",
+    adj, " wife."
+  ]
 
--- test/Tests/Ch15.hs
+++ test/Tests/Ch15.hs
@@ -2,15 +2,32 @@ module Tests.Ch15 where
 
 import Ch15
 import Data.Monoid
-import Test.Tasty (TestTree)
+import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
 
 tests :: [ TestTree ]
-tests =
-  [ testCase "test1" $ Only (Sum 1) `mappend` Only (Sum 1)         @=? Only (Sum 2)
-  , testCase "test2" $ Only (Product 4) `mappend` Only (Product 2) @=? Only (Product 8)
-  , testCase "test3" $ Only (Sum 1) `mappend` Nada                 @=? Only (Sum 1)
-  , testCase "test4" $ Only [1] `mappend` Nada                     @=? Only [1]
-  , testCase "test5" $ Nada `mappend` Only (Sum 1)                 @=? Only (Sum 1)
+tests = [
+    testGroup "15.10" [
+      testCase "test1" $ Only (Sum 1) `mappend` Only (Sum 1)         @=? Only (Sum 2)
+    , testCase "test2" $ Only (Product 4) `mappend` Only (Product 2) @=? Only (Product 8)
+    , testCase "test3" $ Only (Sum 1) `mappend` Nada                 @=? Only (Sum 1)
+    , testCase "test4" $ Only [1] `mappend` Nada                     @=? Only [1]
+    , testCase "test5" $ Nada `mappend` Only (Sum 1)                 @=? Only (Sum 1)
+    ],
+    testGroup "15.11" [
+      testCase "madlibbin'" $
+        madlibbin' "Wow" "loudly" "seat" "second" @=?
+          "Wow! he said loudly as he jumped into his car seat and drove off with his second wife."
+      ,
+
+      testCase "madlibbinBetter'" $
+        madlibbinBetter' "Wow" "loudly" "seat" "second" @=?
+          "Wow! he said loudly as he jumped into his car seat and drove off with his second wife."
+      ,
+
+      testProperty "madlibbin' == madlibbinBetter'" $
+        \e adv noun adj -> (madlibbin' e adv noun adj) == (madlibbinBetter' e adv noun adj)
+    ]
   ]
 
